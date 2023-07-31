3d1723c372cbc447585641c8fc61a89a93a4b93f
Ademola <adeakanfea@gmailcom>
2023-07-31 11:48:54 +0100

    Configuring the worker

--- worker/index.js
+++ worker/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const key = require("./keys");
 const { createClient } = require("redis");
 const Redis = require("./db");
-const crypto = require("crypto");
 
 const app = express();
 
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const client = new Redis();
-const redisClient = client.createClient();
 
 client
   .connect()
@@ -37,9 +35,8 @@ sub
 app.post("/", (req, res) => {
   try {
     const { value, id } = req.body;
-
     client.db_instance.hSet("values", id, value);
-    sub.publish("insert", value);
+    sub.publish("insert", value.toString());
     res.status(200).json({ message: "successfull" });
   } catch (error) {
     res.status(200).json({ message: error.message });
@@ -58,13 +55,13 @@ app.get("/", async (req, res) => {
 app.put("/", async (req, res) => {
   try {
     const { id } = req.body;
-    client.db_instance.hDel("values", id) ;
+    client.db_instance.hDel("values", id);
     res.status(200).json({ message: "done" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("connected to " + process.env.PORT);
+app.listen(5000, () => {
+  console.log("connected to " + 5000);
 });
